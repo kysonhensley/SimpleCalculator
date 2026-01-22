aeafea038a94ed9fca980ef1afbd9fac8fa2985f
kysonhensley <hensleykyson@gmail.com>
2026-01-22 08:52:53 -0700

    recieve user input to consuct selected operation

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,14 +6,24 @@
         {
             int numberOne = 0;
             int numberTwo = 0;
-            int userInput = 0; 
+            string userInput = 0; 
 
             Console.WriteLine("Choose a  number:");
-            numberOne = Console.ReadLine();
+            numberOne = int.Parse(Console.ReadLine());
             Console.WriteLine("Choose a number:");
-            numberTwo = Console.ReadLine();
+            numberTwo = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Choose one of the following options:");
+            Console.WriteLine("1. Sum");
+            Console.WriteLine("2. Product");
+            Console.ReadLine();
+            userInput = Console.ReadLine();
 
+            switch (userInput)
+            {
+                case "1":
+
+            }
 
         } 
     }
